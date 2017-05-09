7316a5ba027e5c6816063871e5a97134ca2a3cf9
christiankemp <chriskemp1992@gmail.com>
2017-05-08 20:50:29 -0700

    Update catalog.js

--- GitHub Project/catalog.js
+++ GitHub Project/catalog.js
@@ -1,4 +1,4 @@
 var records = [
-{brand:"enter a brand here", size:"enter a size here", color:"enter main color(s) here", photo:"photo link here", type: "type of item"},
-{brand:"enter a brand here", size:"enter a size here", color:"enter main color(s) here", photo:"photo link here", type: "type of item"}
+{brand:"enter a brand here", catagory: "item catagory", size:"enter a size here", color:"enter main color(s) here", description: "item description", photo:"photo link here"},
+{brand:"enter a brand here", catagory: "item catagory", size:"enter a size here", color:"enter main color(s) here", description: "item description", photo:"photo link here"}
 ];
