03003ea5e2edd2bec3f984ff2bdabd65916d5a8c
christiankemp <chriskemp1992@gmail.com>
2017-05-08 21:02:37 -0700

    Update catalog.js

--- GitHub Project/catalog.js
+++ GitHub Project/catalog.js
@@ -1,4 +1,6 @@
 var records = [
-{brand:"enter a brand here", catagory: "item catagory", size:"enter a size here", color:"enter main color(s) here", description: "item description", photo:"photo link here"},
-{brand:"enter a brand here", catagory: "item catagory", size:"enter a size here", color:"enter main color(s) here", description: "item description", photo:"photo link here"}
+{brand:"Black Scale", catagory: "Headwear", size:"OS", color:"Black, White", description: "Pitchfork Bandana Strapback", photo:"photo link here"},
+{brand:"RSVP Gallery", catagory: "Hoodie", size:"M", color:"Black, White", description: "RSVP Logo Hoodie", photo:"photo link here"}
+{brand:"424 On Fairfax", catagory:"Shirt", size:"M", color:"Black, Grey", description: "Throwover Flannel Shirt", photo:"photo link here"}
+{brand:"enter a brand here", catagory: "item catagory", size:"enter a size here", color:"enter main color(s) here", description: "item description", photo:"photo link here"}  
 ];
