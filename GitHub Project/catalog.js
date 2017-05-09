7c2f8877afeca2d6c26cf33f8b69d40df486255f
shayk89 <shaykemp89@gmail.com>
2017-05-08 18:41:10 -0700

    Update catalog.js
    json stub

---
+++ GitHub Project/catalog.js
@@ -0,0 +1,4 @@
+var records = [
+{brand:"enter a brand here", size:"enter a size here", color:"enter main color(s) here", photo:"photo link here"},
+{brand:"enter a brand here", size:"enter a size here", color:"enter main color(s) here", photo:"photo link here"}
+];
