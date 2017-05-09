d3cdc2f47d077126b02624832e33172e5e83225f
christiankemp <chriskemp1992@gmail.com>
2017-05-09 12:53:11 -0700

    Update catalog.js

--- GitHub Project/catalog.js
+++ GitHub Project/catalog.js
@@ -31,4 +31,5 @@ function displayRecords () {
     fullDisplay += '<div class="itemDescription">Description:'+item.Description+'</div>';
     fullDisplay += '</div>';
   }
+  element = document.getElementById(id);
 }
